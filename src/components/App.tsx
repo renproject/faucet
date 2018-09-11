3f8da48dcb107fdcc646b4f28c095eafd06cf198
Negaex <gh@negaex.com>
2018-09-11 22:19:47 +1000

    Added cache check

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,15 @@
 import * as React from "react";
 
+import axios from "axios";
+
 import { HashRouter } from "react-router-dom";
 
 import Faucet from "./Faucet";
 import Unlock from "./Unlock";
 
 import "../styles/App.css";
+
+const commitHash = require("../commitHash.json");
 
 interface AppProps {
 
@@ -14,6 +18,7 @@ interface AppProps {
 interface AppState {
     ADDRESS: string | null;
     PRIVATE_KEY: string | null;
+    outOfDate: boolean;
 }
 
 class App extends React.Component<AppProps, AppState> {
@@ -22,11 +27,29 @@ class App extends React.Component<AppProps, AppState> {
         this.state = {
             ADDRESS: null,
             PRIVATE_KEY: null,
+            outOfDate: false,
         };
     }
 
+    public async componentDidMount() {
+        let using;
+        let latest;
+        try {
+            using = commitHash.HASH;
+            latest = (await axios.get(`./commitHash.json?v=${Math.random().toString(36).substring(7)}`)).data.HASH;
+            console.log(latest);
+            console.log(using);
+            if (using !== latest) {
+                this.setState({ outOfDate: true });
+            }
+        } catch (err) {
+            console.log(`Using commit hash ${using} but latest commit hash is ${latest}`);
+            console.error(err);
+        }
+    }
+
     public render() {
-        const { ADDRESS, PRIVATE_KEY } = this.state;
+        const { ADDRESS, PRIVATE_KEY, outOfDate } = this.state;
         // tslint:disable:jsx-no-lambda
         return (
             <div className="App">
@@ -34,7 +57,10 @@ class App extends React.Component<AppProps, AppState> {
                     <div className="app">
                         {PRIVATE_KEY === null || ADDRESS === null ?
                             <Unlock unlockCallback={this.unlockCallback} /> :
-                            <Faucet ADDRESS={ADDRESS} PRIVATE_KEY={PRIVATE_KEY} />
+                            <>
+                                {outOfDate ? <OutOfDate /> : null}
+                                <Faucet ADDRESS={ADDRESS} PRIVATE_KEY={PRIVATE_KEY} />
+                            </>
                         }
                         {/* <Route path="/" exact render={() => <Faucet ADDRESS={ADDRESS} PRIVATE_KEY={PRIVATE_KEY} />} /> */}
                     </div>
@@ -47,6 +73,10 @@ class App extends React.Component<AppProps, AppState> {
     public unlockCallback = (ADDRESS: string, PRIVATE_KEY: string) => {
         this.setState({ ADDRESS, PRIVATE_KEY });
     }
+}
+
+class OutOfDate extends React.Component {
+    public render = () => <div className="outOfDate">This page is out of date. Force refresh the window to update the page (Ctrl-Shift-R or Cmd-Shift-R).</div>;
 }
 
 export default App;
