03c2e60cfaa9112811a9c06b9859a11eae704f97
noiach <gh@noiach.com>
2019-08-05 10:50:24 +1000

    Added debug log

--- src/lib/zec.ts
+++ src/lib/zec.ts
@@ -43,47 +43,52 @@ export const sendRawTransaction = async (txHex: string, mercuryURL: string, chai
 // }
 
 export const transferZEC = async (rawPrivateKey: string, gatewayAddress: string, amountSatoshis: BigNumber) => {
-    console.log(`Please deposit ${amountSatoshis.div(new BigNumber(10).pow(8)).toFixed()} ZEC to ${gatewayAddress}`);
+    try {
+        console.log(`Please deposit ${amountSatoshis.div(new BigNumber(10).pow(8)).toFixed()} ZEC to ${gatewayAddress}`);
 
-    const privateKey = privateToZECAddress(rawPrivateKey).privateKey.toWIF();
+        const privateKey = privateToZECAddress(rawPrivateKey).privateKey.toWIF();
 
-    // const alice = bitcoin.ECPair.fromPrivateKeyBuffer(Buffer.from(keyHex, "hex"), );
-    const account = bitcoin.ECPair.fromWIF(privateKey, bitcoin.networks.zcashTest);
+        // const alice = bitcoin.ECPair.fromPrivateKeyBuffer(Buffer.from(keyHex, "hex"), );
+        const account = bitcoin.ECPair.fromWIF(privateKey, bitcoin.networks.zcashTest);
 
-    const utxos = await getZcashUTXOs(account.getAddress().toString(), 0);
+        const utxos = await getZcashUTXOs(account.getAddress().toString(), 0);
 
-    const fees = new BigNumber(10000);
+        const fees = new BigNumber(10000);
 
-    const tx = new bitcoin.TransactionBuilder(bitcoin.networks.zcashTest);
-    tx.setVersion(bitcoin.Transaction.ZCASH_SAPLING_VERSION);  // 4
-    tx.setVersionGroupId(parseInt("0x892F2085", 16));
+        const tx = new bitcoin.TransactionBuilder(bitcoin.networks.zcashTest);
+        tx.setVersion(bitcoin.Transaction.ZCASH_SAPLING_VERSION);  // 4
+        tx.setVersionGroupId(parseInt("0x892F2085", 16));
 
-    // Add up balance
-    const availableSatoshis = utxos.reduce((sum, utxo) => sum.plus(utxo.value), new BigNumber(0));
+        // Add up balance
+        const availableSatoshis = utxos.reduce((sum, utxo) => sum.plus(utxo.value), new BigNumber(0));
 
-    if (availableSatoshis.lt(amountSatoshis.plus(fees))) {
-        throw new Error("Insufficient balance to broadcast transaction");
+        if (availableSatoshis.lt(amountSatoshis.plus(fees))) {
+            throw new Error("Insufficient balance to broadcast transaction");
+        }
+
+        const change = availableSatoshis.minus(amountSatoshis).minus(fees);
+
+        // Add all inputs
+        utxos.map(utxo => {
+            tx.addInput(utxo.txid, utxo.output_no);
+        });
+
+        // Add outputs
+        tx.addOutput(gatewayAddress, amountSatoshis.toNumber());
+        if (change.gt(0)) { tx.addOutput(account.getAddress(), change.toNumber()); }
+
+        console.log(`${chalk.magenta(`[INFO]`)} ${account.getAddress()} has ${chalk.magenta(`${change.div(new BigNumber(10).pow(8)).toFixed()}`)} tZEC remaining`);
+
+        // Sign inputs
+        utxos.map((utxo, i) => {
+            tx.sign(i, account, "", bitcoin.Transaction.SIGHASH_SINGLE, utxo.value);
+        });
+
+        const built = tx.build();
+
+        await sendRawTransaction(built.toHex(), "http://139.59.217.120:5000/zec/testnet", "ZECTEST");
+    } catch (error) {
+        console.error(error);
+        throw error;
     }
-
-    const change = availableSatoshis.minus(amountSatoshis).minus(fees);
-
-    // Add all inputs
-    utxos.map(utxo => {
-        tx.addInput(utxo.txid, utxo.output_no);
-    });
-
-    // Add outputs
-    tx.addOutput(gatewayAddress, amountSatoshis.toNumber());
-    if (change.gt(0)) { tx.addOutput(account.getAddress(), change.toNumber()); }
-
-    console.log(`${chalk.magenta(`[INFO]`)} ${account.getAddress()} has ${chalk.magenta(`${change.div(new BigNumber(10).pow(8)).toFixed()}`)} tZEC remaining`);
-
-    // Sign inputs
-    utxos.map((utxo, i) => {
-        tx.sign(i, account, "", bitcoin.Transaction.SIGHASH_SINGLE, utxo.value);
-    });
-
-    const built = tx.build();
-
-    await sendRawTransaction(built.toHex(), "http://139.59.217.120:5000/zec/testnet", "ZECTEST");
 };
